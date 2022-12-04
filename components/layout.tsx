d12437966a3135f9f8b4d7c480fdd2b91e11ed1f
Dragan Smiljanic <dragansmiljanic@gmail.com>
2022-12-04 13:58:36 +0100

    Fix Layout component signature

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,9 @@
-const Layout: React.FC = ({ children }) => {
+interface WithChildren {
+  children: React.ReactNode;
+}
+
+
+const Layout: React.FC<WithChildren> = ({ children }) => {
   return <div className='max-w-3xl px-5 py-5 mx-auto md:px-0 md:py-10'>
     {children}
   </div>
