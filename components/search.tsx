c9392332d03e883cded9db2828949c8569c088fe
Dragan Smiljanic <dragansmiljanic@gmail.com>
2022-12-04 17:19:00 +0100

    Debounce search component change event

--- components/search.tsx
+++ components/search.tsx
@@ -1,4 +1,5 @@
 import { ChangeEventHandler, FC } from 'react';
+import debounce from '../utils/debounce';
 
 type SearchProps = {
   onChange: (value: string) => void;
@@ -18,7 +19,7 @@ const Search: FC<SearchProps> = ({ onChange }) => {
         id="query"
         className="focus:ring-blue-600 focus:border-blue-600 block w-full sm:text-sm border-gray-300 text-gray-800 rounded bg-gray-50 p-3"
         placeholder="Search by name, IATA, city, or country"
-        onChange={handleOnChange}
+        onChange={debounce(handleOnChange)}
       />
     </div>
   );
